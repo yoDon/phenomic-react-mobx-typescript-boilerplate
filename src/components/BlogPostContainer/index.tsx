8a0aeccb63a0e81965569b5766998899c680b589
Don Alvarez <don@acceleratedpictures.com>
2017-07-03 12:57:43 -0700

    Adding strongly typed props for routable tags

--- src/components/BlogPostContainer/index.tsx
+++ src/components/BlogPostContainer/index.tsx
@@ -1,8 +1,22 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import * as ReactRouter from 'react-router'
+import * as H from 'history'
 import { createContainer, query, BodyRenderer } from '@phenomic/preset-react-app/lib/client';
 
-const BlogPost = (props:any) => {
+interface RouterProvidedContainerProps {
+  children:React.ReactElement<any>,
+  location:H.Location,
+  router:H.History,
+  route:ReactRouter.PlainRoute,
+  routes:ReactRouter.PlainRoute[],
+  params:any,
+  routeParams:any,
+  page:{status:string, node:any},
+  isLoading:boolean
+}
+
+const BlogPost = (props:RouterProvidedContainerProps) => {
   return (
     <div>
       {props.page.node &&
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,17 +1,29 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import * as ReactRouter from 'react-router'
+import * as H from 'history'
 import TimerButton from '../../components/TimerButton'
 
-class Home extends React.Component<{}, {}> {
+interface RouterProvidedProps {
+  children:React.ReactElement<any>,
+  location:H.Location,
+  router:H.History,
+  route:ReactRouter.PlainRoute,
+  routes:ReactRouter.PlainRoute[],
+  params:any,
+  routeParams:any
+}
+
+class Home extends React.Component<RouterProvidedProps, {}> {
 
   render() {
     return (
       <div>
         <TimerButton/>
       </div>
-    );
+    )
   }
 
 }
 
-export default Home;
+export default Home
